7a70618003ad006ba7b5d8b833d170a982ed1e5b
Ahmed Walid <ahmed.ibrahim003@eng-st.cu.edu.eg>
2022-09-12 00:12:18 +0200

    switching between players

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
 "use strict";
 
 // DOM ELEMENTS
-const score1_ = document.querySelector('.player-1-score');
-const score2_ = document.querySelector('.player-2-score');
-const dice_   = document.querySelector('.dice'); 
+const score0_ = document.querySelector(".player-0-score");
+const score1_ = document.querySelector(".player-1-score");
+const dice_ = document.querySelector(".dice");
+const btnRoll_ = document.querySelector(".roll");
+const btnNew_ = document.querySelector(".new");
+const btnHold_ = document.querySelector(".hold");
+const currScore0_ = document.querySelector(".player-0-currscore");
+const currScore1_ = document.querySelector(".player-1-currscore");
+const player0_ = document.querySelector(".player--0");
+const player1_ = document.querySelector(".player--1");
 
+const currScores_ = [currScore0_, currScore1_];
+const scores_ = [score0_, score1_];
+const players_ = [player0_, player1_];
+
+
+// Players' score
+let scores = [0, 0];
+let currScore  = 0;
+let currPlayer = 0;
 
 initNewGame();
+btnRoll_.addEventListener("click", rollDice);
 
-function initNewGame(){
-    score1_.textContent = '0';
-    score2_.textContent = '0';
-    dice_.classList.add('hidden');
+function initNewGame() {
+  scores [0] = 0;
+  currScores_[0].textContent = "0";
+  scores_[0].textContent = "0";
+  scores [1] = 0;
+  scores_[1].textContent = "0";
+  currScores_[1].textContent = "0";
+  dice_.classList.add("hidden");
 }
 
+function rollDice() {
+  // Roll the dice
+  const dice = Math.trunc(Math.random() * 6) + 1;
 
+  // Display the dice
+  dice_.src = `images/dice-${dice}.png`;
+  dice_.classList.remove("hidden");
 
+  //check if the rolled number is 1
+  if (dice !== 1) {
+    currScore += dice;
+    currScores_[currPlayer].textContent = currScore;
+    // TODO: check if the current player has won.
+    if(scores[currPlayer] + currScore >= 100){
+        // the current player has won.
+        console.log(`Player number ${currPlayer + 1} has won`);
+    }
 
+  } else {
+    // switch to the other player.
+    players_[currPlayer].classList.remove("active-player");
+    players_[currPlayer].classList.add("player");
+    scores[currPlayer] += currScore;
+    scores_[currPlayer].textContent = scores[currPlayer];
+    currScores_[currPlayer].textContent = "0";
+    currScore = 0;
+    currPlayer  = Number(!currPlayer);
+    players_[currPlayer].classList.add("active-player");
+    players_[currPlayer].classList.remove("player");
+  }
+}
